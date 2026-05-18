4a8e24e66609312512a90241725186ed2254cd49
hzw20070903 <2495640991@qq.com>
2026-05-18 22:30:49 +0800

    Add files via upload

---
+++ 迭代法求最小公倍数.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int arr(int a,int b)
+{
+int	c=a;
+	int d;
+	while(1)
+	{c++;
+	   if(c%a==0&&c%b==0)
+	   {d=c;
+	   	break;
+	   	
+	   	
+	   	
+	   }
+	}
+	return d;
+}
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++)
+	{
+	
+    }
+	
+	
+	
+	
+	
+	return 0;
+}
+long long result = 1;              ///////
+    for (int i = 2; i <= n; i++) {////////
+        result = lcm(result, i);//////
+    }
