50496418c754b09beae9660cb27ff6e93dd423e3
hzw20070903 <2495640991@qq.com>
2026-05-18 22:52:01 +0800

    Update 迭代法求最小公倍数.cpp

--- 迭代法求最小公倍数.cpp
+++ 迭代法求最小公倍数.cpp
@@ -30,7 +30,7 @@ int main()
 	
 	return 0;
 }
-long long result = 1;              ///////
+long long result = 1;              //
     for (int i = 2; i <= n; i++) {//////
         result = lcm(result, i);//////
     }
